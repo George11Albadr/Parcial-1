9ba1a6986bf332923880f8d425d7225e5f6a92eb
George11Albadr <97915760+George11Albadr@users.noreply.github.com>
2022-03-19 14:49:47 -0600

    Update and rename miniMain.py to test_miniMain.py

---
+++ test_miniMain.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 15:43:55 2022
+
+@author: georg
+"""
+
+import unittest
+import miniMain
+
+class TestminiMain(unittest.Testcase):
+    
+    def setUp(self):
+        self.nombre_producto1 = miniMain('pollo')
+        self.cantidad_producto1 = miniMain(90)
+    
+    def tearDown(self): 
+        pass
+    
+    def test_agregarInventario(self):
+        
+        self.assertEqual(self.ombre_producto1.agregarInventario,'pollo')
+        self.assertEqual(self.cantidad_producto1.agregarInventario, 90)
+        
+if __name__=="__main__":
+    unittest.main()
+    
+        
